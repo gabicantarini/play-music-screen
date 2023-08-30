2e6723d7f65f0d9759d27c8991d3151f1b20cf29
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-08-30 22:52:55 +0100

    added menu options

--- PlayMusicScreen/PlayMusicScreen/Program.cs
+++ PlayMusicScreen/PlayMusicScreen/Program.cs
@@ -5,7 +5,7 @@
 ██████╔╝██║░░░░░███████║░╚████╔╝░  ██╔████╔██║██║░░░██║╚█████╗░██║██║░░╚═╝
 ██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░  ██║╚██╔╝██║██║░░░██║░╚═══██╗██║██║░░██╗
 ██║░░░░░███████╗██║░░██║░░░██║░░░  ██║░╚═╝░██║╚██████╔╝██████╔╝██║╚█████╔╝
-╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░  ╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░╚════╝░" + "\n \n Welcome Play Music!";
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░  ╚═╝░░░░░╚═╝░╚═════╝░╚═════╝░╚═╝░╚════╝░" + "\n \nWelcome to Play Music!";
 
 void ShowWelcomeMessage()
 {
@@ -15,3 +15,21 @@ void ShowWelcomeMessage()
 
 ShowWelcomeMessage();
 
+void ShowMenuOptions()
+{
+    Console.WriteLine("\nEnter 1 to register a new Band: ");
+    Console.WriteLine("Enter 2 to show all our Band");
+    Console.WriteLine("Enter 3 to rate our Band");
+    Console.WriteLine("Enter 4 to show an average Band");
+    Console.WriteLine("Enter -1 to exit");
+
+    Console.Write("Enter your option: ");
+    string ChosenOption = Console.ReadLine()!;
+
+    if(ChosenOption == "1")
+    {
+        Console.WriteLine("You choice were Option 1");
+    }
+}
+
+ShowMenuOptions();
