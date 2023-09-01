7e91c4e4da070013fc3af4eb0861cee3010bc207
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-01 10:20:52 +0100

    added album method

--- PlayMusicOO/PlayMusicOO/Album.cs
+++ PlayMusicOO/PlayMusicOO/Album.cs
@@ -1,4 +1,12 @@
 ﻿class Album
 {
+    private List<Music> musicList = new List<Music>();
+    public string Name { get; set;}
 
+    public int TotalDuration { get; set;} //count the album total duration hour
+
+    public void AddMusic (Music music)
+    {
+        musicList.Add (music); //added music in the album list
+    }
 }
