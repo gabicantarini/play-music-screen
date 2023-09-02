90ceb37fa69ae915cb325c3672e43359763dc55a
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-02 11:23:04 +0100

    added podcast method

--- PlayMusicOO/PlayMusicOO/Podcast.cs
+++ PlayMusicOO/PlayMusicOO/Podcast.cs
@@ -1,4 +1,19 @@
 ﻿class Podcast
 {
+    public Podcast(string name, string host)
+    {
+        Name = name;
+        Host = host;
+    }
+
+    public string Name { get; }
+    public string Host { get; }
+    public int TotalChapter { get; set; }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Poscast name: {Name}, hosted by {Host}.");
+    }
 
 }
+
