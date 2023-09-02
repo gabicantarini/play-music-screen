189a5d2d5fa449c3c93383498543b45d8b0cd1bc
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-02 10:03:06 +0100

    added band method

--- PlayMusicOO/PlayMusicOO/Band.cs
+++ PlayMusicOO/PlayMusicOO/Band.cs
@@ -1,4 +1,12 @@
 ﻿class Band
 {
+    private List<Album> albums = new List<Album>();
+    public string Name { get; set; }
+
+    public void AddAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
 
 }
