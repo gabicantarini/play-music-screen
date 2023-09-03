a03b1f25a72211fb8952cf1125d60531f14f19ab
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-03 10:57:47 +0100

    added new guest method

--- PlayMusicOO/PlayMusicOO/Chapter.cs
+++ PlayMusicOO/PlayMusicOO/Chapter.cs
@@ -1,4 +1,21 @@
 ﻿class Chapter
 {
+    private List<string> newGuest = new();
+      
+    public Chapter(int number, string title)
+    {
+        Number = number;
+        Title = title;
+    }
+
+    public int Number { get; }
+    public string Title { get; set; }
+    public int Duration { get; set; }
+    public string Summary => $"The today´s chapter is number {Number}, {Title} and duration {Duration}.";
+
+    public void AddGuest(string guest)
+    {
+        newGuest.Add(guest); //added music in the album list
+    }
 
 }
